10884a548025d8c2af50a98075f1f3d993fef946
Sara Elhawary <elhawary.sara92@gmail.com>
2022-03-26 11:45:55 +0200

    connect to cloud DB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,47 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-const bodyParser = require("body-parser")
-
+const morgan = require('morgan')
 const port = 3000
+const api = process.env.API
 
 app.use(express.static('./client/public'))
-app.use(bodyParser.json())
-require('./db')
+app.use(express.json())
+app.use(morgan('tiny'))
 
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'ECOMMERCE'
+}).then(() => {
+    console.log("connecting to database....")
 
+}).catch((err) => {
+    console.log(err)
+    process.exit(1)
+})
+
+app.get(`${api}/`, (req, res) => {
+    const product = {
+        id: 1,
+        prod_name: 'phone',
+        image: 'url'
+    }
+    res.send(product);
+
+})
+
+app.post(`${api}/`, (req, res) => {
+    const prod = req.body
+    console.log(prod)
+    res.send(prod);
+
+})
 
 
 app.listen(port, () => {
+    console.log(api)
     console.log(`Example app listening on port ${port}`)
 
 })
