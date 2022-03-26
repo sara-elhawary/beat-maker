63fb3ade8d79161d55557ecff87f122319ffacaa
Sara Elhawary <elhawary.sara92@gmail.com>
2022-03-26 10:51:02 +0200

    start over

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser")
-// const projectRouter = require('./projects/projectRouter')
-// const userRouter = require('./Users/userRouter')
-const cartRouter = require('./carts/CartRouter')
+
 const port = 3000
 
 app.use(express.static('./client/public'))
 app.use(bodyParser.json())
-
-// app.use('/project', projectRouter)
-// app.use('/login', userRouter)
-app.use('/cart', cartRouter)
-
 require('./db')
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
