129670285779357436e443030cf79b2725d6a2a7
Sara Elhawary <elhawary.sara92@gmail.com>
2022-03-28 16:02:10 +0200

    fix authentication bug

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -4,20 +4,21 @@ module.exports = () => {
     const secret = process.env.TOKEN_SECRET
     return expressJWT({
         secret,
-        algorithms: ['HS256']
+        algorithms: ['HS256'],
+        isRevoked: async (req, payload, done) => {
+            if (!payload.isAdmin) {
+                done(null, true)
+            }
+            done()
+        }
     })
         .unless({
             path: [
-                {
-                    url: '/\/api\/v1\/products(.*)/'
-                    , method: ["GET", "OPTIONS"]
-                },
-                {
-                    url: '/\/api\/v1\/categories(.*)/'
-                    , method: ["GET", "OPTIONS"]
-                },
+                { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+                { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
                 '/api/v1/users/register',
                 '/api/v1/users/login'
             ]
         })
 }
+
