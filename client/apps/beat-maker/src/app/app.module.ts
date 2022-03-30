649a3e3a29a97e8798ec6eca2fdf47d23a118130
Sara Elhawary <elhawary.sara92@gmail.com>
2022-03-30 22:25:55 +0200

    setup shared styling

--- client/apps/beat-maker/src/app/app.module.ts
+++ client/apps/beat-maker/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -9,6 +9,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { UiModule } from '@client/ui';
+import {ProgressBarModule} from 'primeng/progressbar';
+
 
 const routes:Routes=[
   {path:'',component:HomePageComponent},{path:"products",component:ProductListComponent},
@@ -16,7 +18,7 @@ const routes:Routes=[
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule,RouterModule.forRoot(routes),UiModule],
+  imports: [BrowserModule,BrowserAnimationsModule,RouterModule.forRoot(routes),UiModule,ProgressBarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
